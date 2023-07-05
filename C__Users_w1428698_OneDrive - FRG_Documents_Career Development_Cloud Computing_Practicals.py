962b253f33dd65750e4055ad9b253bd7778b07d7
Nsikelelo <44255998+Nsikelelo@users.noreply.github.com>
2023-07-05 21:57:46 +0200

    Add files via upload

---
+++ C__Users_w1428698_OneDrive - FRG_Documents_Career Development_Cloud Computing_Practicals.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#code cell
+print ("Hello World")
+
+
+# # This is a markdown 1st try
+# This is first attempt of trying out a tech discussion by writting a one line of python code and this markdown.
+# ## Testing headings
+# 
+# **Testing Bold**
+# _Testing Italic_
+# ***testing both bold and italic***
+# ~~testing Strikethorugh text~~
+# 
+# 
+# 
+
+# In[ ]:
+
+
+
+
